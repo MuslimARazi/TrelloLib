4b003661642dffbdfda8c5e17ecd8c8da1de0541
Jacob <zexagonal.selvo@gmail.com>
2018-06-26 19:14:09 -0700

    GetBoardID added

--- Source/trelloLib.js
+++ Source/trelloLib.js
@@ -2,3 +2,21 @@ var request = require("request-promise");
 
 var TrelloKey = "TRELLOKEY"
 var TrelloToken = "TRELLOTOKEN"
+
+exports.GetBoardID = async function(id){
+    var BoardID;
+
+    var options = { 
+        method: 'GET',
+        url: `https://api.trello.com/1/boards/${id}`,
+        key: TrelloKey,
+        token: TrelloToken
+    };
+
+    await request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        BoardID = JSON.parse(body).id
+    });
+
+    return BoardID
+}
